10725cf38d6362bffee8e7730631d82f0a181003
Enzo D <enzo.danjour@gmail.com>
2022-02-14 23:18:14 -0300

    add fetch api

--- src/pages/noticia.jsx
+++ src/pages/noticia.jsx
@@ -1,5 +1,21 @@
-export function Noticia(){
-    return<h1>
-        Enzo
+import { useEffect, useState } from "react"
+
+export function Noticia() {
+    const [weater, setWeater]= useState([]);
+
+    useEffect(()=>{
+        fetch('http://api.weatherapi.com/v1/current.json?key=51b4d1e551d146e88f613942221502&q=189.87.83.242&aqi=yes')
+    .then(async (response) => {
+        const json = await response.json();
+        console.log('response', json.current.condition.text);
+        setWeater(json.current.condition.text);
+    })
+    .catch((error) => {
+        console.log('erro', error);
+    });
+    },[]);
+    return <h1>
+        {weater}
     </h1>
 }
+
