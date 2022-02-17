d3b3b0907045efa509b73058c56f8bd3c53317d7
Enzo D <enzo.danjour@gmail.com>
2022-02-16 22:15:58 -0300

    add noticia

--- src/pages/noticia.jsx
+++ src/pages/noticia.jsx
@@ -1,21 +1,33 @@
 import { useEffect, useState } from "react"
+import { Col, Row } from "react-bootstrap";
 
 export function Noticia() {
-    const [weater, setWeater]= useState([]);
-
-    useEffect(()=>{
+    const [weater, setWeater] = useState([]);
+    const [image, setImage] = useState([]);
+    useEffect(() => {
         fetch('http://api.weatherapi.com/v1/current.json?key=51b4d1e551d146e88f613942221502&q=189.87.83.242&aqi=yes')
-    .then(async (response) => {
-        const json = await response.json();
-        console.log('response', json.current.condition.text);
-        setWeater(json.current.condition.text);
-    })
-    .catch((error) => {
-        console.log('erro', error);
-    });
-    },[]);
-    return <h1>
-        {weater}
-    </h1>
+            .then(async (response) => {
+                const json = await response.json();
+                console.log('response', json.current.condition.icon);
+                setImage(json.current.condition.icon)
+                setWeater(json.current.condition.text);
+            })
+            .catch((error) => {
+                console.log('erro', error);
+            });
+    }, []);
+    return (
+        <>
+            <div style={{ display: "grid", gridTemplateColumns: "1fr 20fr" }}>
+                <img src={image} />
+                <span>
+                    {weater}
+                </span>
+            </div>
+
+
+
+        </>
+    )
 }
 
