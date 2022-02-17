687c32622bd3753fdddf8061bba9a250dcdbc300
Enzo D <enzo.danjour@gmail.com>
2022-02-17 19:17:24 -0300

    update path

---
+++ src/components/Nave.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export function Nave() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">Sig-Tads</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/">Principal</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/noticia">Noticia</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/Projeto">Projeto</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled">Calendário</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled">API</a>
+            </li>
+
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
