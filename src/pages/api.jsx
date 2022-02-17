651cb97ad35320c020a64ad0c9f638265d579a5d
Enzo D <enzo.danjour@gmail.com>
2022-02-17 19:52:28 -0300

    add page

---
+++ src/pages/api.jsx
@@ -0,0 +1,35 @@
+
+  
+import { useEffect, useState } from "react"
+export function Api() {
+    const [weater, setWeater] = useState([]);
+    const [image, setImage] = useState([]);
+    useEffect(() => {
+        fetch('http://api.weatherapi.com/v1/current.json?key=51b4d1e551d146e88f613942221502&q=189.87.83.242&aqi=yes')
+            .then(async (response) => {
+                const json = await response.json();
+                console.log('response', json.current.condition.icon);
+                setImage(json.current.condition.icon)
+                setWeater(json.current.condition.text);
+            })
+            .catch((error) => {
+                console.log('erro', error);
+            });
+    }, []);
+    return (
+        <>
+        <h1>
+            Clima de Macaíba
+        </h1>
+            <div style={{ display: "grid", gridTemplateColumns: "1fr 20fr" }}>
+                <img src={image} />
+                <span>
+                    {weater}
+                </span>
+            </div>
+
+
+
+        </>
+    )
+}
