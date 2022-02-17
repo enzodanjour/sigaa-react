16fcacaf364b5a250f2c76b7dd857a3eb080ee1e
Enzo D <enzo.danjour@gmail.com>
2022-02-17 19:50:37 -0300

    update nav

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,34 +1,34 @@
 import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-  } from "react-router-dom";
-import AppFooter from "./components/AppFooter";
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 import { Principal } from "./pages/principal";
 import { Projeto } from "./pages/projeto";
-import { Noticia} from "./pages/noticia";
-import {Calendario} from "./pages/calendario";
-import {Api} from "./pages/api";
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Nave from "./Nave";
+import { Calendario } from "./pages/calendario";
+import { Api } from "./pages/api";
+import { Nave } from "./components/Nave";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import AppFooter from "./components/AppFooter";
+import { Noticia } from "./pages/noticia";
 
-  export function AppRoutes(){
-    return (
-      <div>
-        <Router>
-          <Nave/>
-              <Switch> 
-                <Route path="/" element={<Principal/>}/>  
-                <Route path= "/noticia" element={<Noticia/>}/>
-                <Route path="/projeto" element={<Projeto/>}/> 
-                <Route path="/calendario" element={<Calendario/>}/> 
-                <Route path="/api" element={<Api/>}/> 
-              </Switch>
-          <AppFooter/>
-        </Router>
-        
-      </div>
-        
-    )
-  }
+export function AppRoutes() {
+  return (
+    <div>
+      <Nave />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Principal />} />
+          <Route path="/noticia" element={<Noticia />} />
+          <Route path="/projeto" element={<Projeto />} />
+          <Route path="/calendario" element={<Calendario />} />
+          <Route path="/api" element={<Api />} />
+
+
+        </Routes>
+      </Router>
+      <AppFooter />
+    </div>
+
+  )
+}
