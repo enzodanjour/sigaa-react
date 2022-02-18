ee883d730d3bb080201ad8c20ddd564023dac0cc
Enzo D <enzo.danjour@gmail.com>
2022-02-18 00:02:05 -0300

    updates

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // css: { preprocessorOptions: { scss: { charset: false } } },
-  base:'/sigaa-react',
+  base:'/sigaa-react/',
   plugins: [react()]
 })
